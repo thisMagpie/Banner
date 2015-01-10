9361c99749d4414c83dc2f8c5956d483941664b7
Magdalen Berns <m.berns@thismagpie.com>
2015-01-10 16:47:35 +0000

    Create iframe layout and print to html file
    Also opens browser

--- IndiGoGo.py
+++ IndiGoGo.py
@@ -5,10 +5,47 @@ from urllib import urlopen
 
 URL = 'https://www.indiegogo.com/project/builder-an-ide-of-our-gnome/embedded/8947753'
 
+# Define variables
 f = urlopen(URL)
 data = f.read()
 soup = BeautifulSoup(data, 'html.parser')
 amount = soup.find(**{'class': 'currency currency-medium'}).span.text
-fraction = int(''.join([c for c in amount if c in '0123456789'])) / 30000.
 
-print ('''{"raised": "%s", "fraction": %f}''' % (amount, fraction))
+body_open_tag = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type">
+  <title>Builder Fundraiser</title>
+</head>
+<body align=center>'''
+
+heading='''<h1>GNOME Builder Fundraiser.</h1>
+           <h2><a href="https://www.indiegogo.com/project/builder-an-ide-of-our-gnome/">Please Donate!</a></h2>'''
+
+raised_open_tag ='''<h3>'''
+raised_close_tag =''' Raised So Far!</h3>'''
+
+body_close_tag='''
+</body>
+</html>
+'''
+
+contents = body_open_tag + heading + raised_open_tag + amount + raised_close_tag + body_close_tag
+
+def main():
+  local_browser(contents)
+
+def string_to_file(text, filename):
+    """Write a file with the given name and the given text."""
+    output = open(filename,"w")
+    output.write(text)
+    output.close()
+
+def local_browser(webpageText, filename='temp.html'):
+    '''File at '''
+    import webbrowser, os.path
+    string_to_file(webpageText, filename)
+    webbrowser.open("file:///" + os.path.abspath(filename))
+
+main()
