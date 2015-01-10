8ea288cdd04a8f8b1540540d18d2efc4167d2ce7
Magdalen Berns <m.berns@thismagpie.com>
2015-01-10 21:33:16 +0000

    Add styling and image

--- IndiGoGo.py
+++ IndiGoGo.py
@@ -18,11 +18,11 @@ body_open_tag = '''<!DOCTYPE html">
 </head>
 <body align=center>'''
 
-heading='''<h1>GNOME Builder Fundraiser.</h1>
-           <h2><a href="https://www.indiegogo.com/projects/builder-an-ide-of-our-gnome">Please Donate!</a></h2>'''
-
-raised_open_tag ='''<h3>'''
-raised_close_tag =''' Raised So Far!</h3>'''
+heading='''
+<img src="https://wiki.gnome.org/Apps/Builder?action=AttachFile&do=get&target=splash.png"><img></br>
+<button style="font-family:Charcoal, sans-serif; padding: 20px 100px;background: #f9f9f9; color: #000; font-size:16px; border-radius:6px;"><a href="https://www.indiegogo.com/projects/builder-an-ide-of-our-gnome">Donate</a></button>'''
+raised_open_tag ='''<p style="font-family:Impact, Charcoal, sans-serif">'''
+raised_close_tag =''' Raised So Far!</p>'''
 
 body_close_tag='''
 </body>
