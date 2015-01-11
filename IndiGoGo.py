274e8e21e99107ed4609e436d30b3bb02ea0130e
Magdalen Berns <m.berns@thismagpie.com>
2015-01-11 02:18:00 +0000

    Add progress bar

--- IndiGoGo.py
+++ IndiGoGo.py
@@ -13,6 +13,14 @@ amount = soup.find(**{'class': 'currency currency-medium'}).span.text
 
 body_open_tag = '''<!DOCTYPE html">
 <html>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <head>
   <title>Builder Fundraiser</title>
 </head>
@@ -20,9 +28,19 @@ body_open_tag = '''<!DOCTYPE html">
 
 heading='''
 <img src="https://wiki.gnome.org/Apps/Builder?action=AttachFile&do=get&target=splash.png"><img></br>
-<button style="font-family:Charcoal, sans-serif; padding: 20px 100px;background: #f9f9f9; color: #000; font-size:16px; border-radius:6px;"><a href="https://www.indiegogo.com/projects/builder-an-ide-of-our-gnome">Donate</a></button>'''
-raised_open_tag ='''<p style="font-family:Impact, Charcoal, sans-serif">'''
-raised_close_tag =''' Raised So Far!</p>'''
+<button style="font-family:Charcoal, sans-serif; padding: 20px 100px;background: #f9f9f9; color: #000; font-size:16px; border-radius:6px;"><a href="https://www.indiegogo.com/projects/builder-an-ide-of-our-gnome">Donate</a></button>
+'''
+
+raised_open_tag ='''<div class="progress" style="width:600px; text-align:center;">
+  <div class="progress-bar progress-bar-success" role="progressbar" style="width:40%">
+  </div>
+  <div class="progress-bar progress-bar-warning" role="progressbar" style="width:20%">
+    Nearly There!
+  </div>
+  <div class="progress-bar progress-bar-danger" role="progressbar" style="width:2%; color:#000;">'''
+raised_close_tag =''' Raised so far!
+  </div>
+</div><p style="font-family:Impact, Charcoal, sans-serif">'''
 
 body_close_tag='''
 </body>
